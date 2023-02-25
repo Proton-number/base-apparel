10a3d6fb370ce98a8b0d55518cc01ece0cf694be
Dacron <favouradebimpe63@gmail.com>
2023-02-25 04:09:28 +0100

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Desktop from './Components/Desktop Designs/Desktop'
 import Mobile from './Components/Mobile Designs/Mobile'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  
 
   return (
     <div className="App">
+        <Desktop />
        <Mobile />
-      <Desktop />
     </div>
   )
 }
